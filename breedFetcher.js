3a4de9deae7499c9135741f86650eb924afd1505
graemenickerson <graeme.nickerson@gmail.com>
2019-09-26 20:11:32 +0000

    Adds functionality to breedFetcher.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,3 +3,18 @@
 // September 26, 2019
 
 const request = require('request');
+const args = process.argv.slice(2);
+
+request(`https://api.thecatapi.com/v1/breeds/search?q=${args}`, (error, response, body) => {
+  if (body) {
+    const data = JSON.parse(body);
+    if (data.length > 0) {
+      console.log(data[0].description);
+    } else {
+      console.error('Invalid Breed Name!');
+    }
+  } else {
+    console.error(error);
+  }
+  
+});
